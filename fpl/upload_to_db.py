929c2a1aa15430fb9c0275864fb40b32daafcecf
Tamhid Chowdhury <tamhidchowdhury@gmail.com>
2025-10-01 15:29:33 +0000

    x

--- fpl/upload_to_db.py
+++ fpl/upload_to_db.py
@@ -2,6 +2,9 @@ import os
 import pandas as pd
 from sqlalchemy import create_engine
 
+print(f"Current working directory: {os.getcwd()}")
+print(f"OS List Dir: {os.listdir(os.getcwd())}")
+
 INPUT_DIR = os.environ.get("INPUT_DIR", "/data/dropbox/ready")
 DB_URL    = os.environ["DB_URL"]
 FILENAME = "preds_merged.csv"
