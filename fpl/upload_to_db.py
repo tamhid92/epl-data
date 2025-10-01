6d5af2eeafa113b06466971cf58041d98ba71a7e
Tamhid Chowdhury <tamhidchowdhury@gmail.com>
2025-10-01 07:02:08 +0000

    initial jenkins

---
+++ fpl/upload_to_db.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+
+INPUT_DIR = os.environ.get("INPUT_DIR", "/data/dropbox/ready")
+DB_URL    = os.environ["DB_URL"]
+FILENAME = "preds_merged.csv"
+
+engine = create_engine(DB_URL)
+
+print(f"Loading {FILENAME} ...")
+df = pd.read_csv(FILENAME)
+df.to_sql("predicted_next_gw", engine, if_exists="replace", index=False)
+
+print("Done.")
