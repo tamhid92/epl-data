e21d99c300c03524a46cd8e87f62e03085c1238a
Tamhid Chowdhury <tamhidchowdhury@gmail.com>
2025-10-01 16:02:52 +0000

    x

--- fpl/upload_to_db.py
+++ fpl/upload_to_db.py
@@ -3,14 +3,15 @@ import pandas as pd
 from sqlalchemy import create_engine
 
 print(f"Current working directory: {os.getcwd()}")
-print(f"OS List Dir: {os.listdir(os.getcwd())}")
 
-INPUT_DIR = os.environ.get("INPUT_DIR", "/data/dropbox/ready")
+
+INPUT_DIR = os.environ.get("INPUT_DIR", "/ready")
 DB_URL    = os.environ["DB_URL"]
 FILENAME = "preds_merged.csv"
 
 engine = create_engine(DB_URL)
 
+print(f"OS List Dir: {os.listdir(INPUT_DIR)}")
 print(f"Loading {FILENAME} ...")
 df = pd.read_csv(FILENAME)
 df.to_sql("predicted_next_gw", engine, if_exists="replace", index=False)
