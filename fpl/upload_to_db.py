2a0dfd962cec69e93fd0d1d5e035e56da7a50837
Tamhid Chowdhury <tamhidchowdhury@gmail.com>
2025-10-01 16:30:21 +0000

    x

--- fpl/upload_to_db.py
+++ fpl/upload_to_db.py
@@ -4,6 +4,14 @@ from sqlalchemy import create_engine
 
 print(f"Current working directory: {os.getcwd()}")
 
+DB_HOST = os.environ.get('DB_HOST')
+DB_PORT = os.environ.get('DB_PORT')
+DB_NAME = os.environ.get('DB_NAME')
+DB_USER = os.environ.get('DB_USER')
+DB_PASS = os.environ.get("DB_PASS")
+
+DB_URL = f"postgresql+psycopg2://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+
 INPUT_DIR = os.environ.get("INPUT_DIR", "/ready")
 DB_URL    = os.environ["DB_URL"]
 print(DB_URL)
