e10cc6590b1d6e9a51b601ee6fabaed1f3eb4f84
Tamhid Chowdhury <tamhidchowdhury@gmail.com>
2025-10-01 16:45:34 +0000

    x

--- fpl/upload_to_db.py
+++ fpl/upload_to_db.py
@@ -9,11 +9,9 @@ DB_PORT = os.environ.get('DB_PORT')
 DB_NAME = os.environ.get('DB_NAME')
 DB_USER = os.environ.get('DB_USER')
 DB_PASS = os.environ.get("DB_PASS")
-
-DB_URL = f"postgresql+psycopg2://${DB_USER}:${DB_PASS}@${DB_HOST}:${DB_PORT}/${DB_NAME}"
+DB_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 INPUT_DIR = os.environ.get("INPUT_DIR", "/ready")
-print(DB_URL)
 FILENAME = "preds_merged.csv"
 
 engine = create_engine(DB_URL)
